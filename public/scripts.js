e9e84bd85dfe3f5e9dd589d1867cb3fd61bdd180
bao <quocbao2116@gmail.com>
2024-02-25 13:11:39 +0700

    add

---
+++ public/scripts.js
@@ -0,0 +1,46 @@
+const multiplier = {
+    translate: .1,
+    rotate: .01
+}
+
+const mySwiper = new Swiper('.swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 90,
+    centeredSlides: true,
+    loop: true,
+    grabCursor: true
+});
+
+function calculateWheel() {
+    const slides = document.querySelectorAll('.single');
+    slides.forEach((slide, i) => {
+        const rect = slide.getBoundingClientRect();
+        const r = window.innerWidth * .5 - (rect.x + rect.width * .5);
+        let ty = Math.abs(r) * multiplier.translate - rect.width * multiplier.translate;
+
+        if (ty < 0) {
+            ty = 0;
+        }
+        const transformOrigin = r < 0 ? 'left top' : 'right top';
+        slide.style.transform = `translate(0, ${ty}px) rotate(${-r * multiplier.rotate}deg)`;
+        slide.style.transformOrigin = transformOrigin;
+    });
+}
+
+function raf() {
+    requestAnimationFrame(raf);
+    calculateWheel();
+}
+
+raf();
+
+$('.btn-previous').on('click', function () {
+    // Simulate dragging to the right (previous)
+    mySwiper.slidePrev();
+});
+
+$('.btn-next').on('click', function () {
+    // Simulate dragging to the left (next)
+    mySwiper.slideNext();
+
+});
